72d6821990a47956c08a473c441daa06947983be
Dean Abbott <deanabbott293@gmail.com>
2021-07-17 18:50:58 +0900

    added associations for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :bookings # bookings as a client
+  has_many :books
+  has_many :bookings_as_owner, through: :books, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
