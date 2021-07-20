51e5c725d431335f825746ff9e8ef33efe1f7181
Dean Abbott <deanabbott293@gmail.com>
2021-07-20 21:30:30 +0900

    added new seeds with assigned users

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,13 @@
  #    t.bigint "user_id", null: false
  #    t.index ["user_id"], name: "index_books_on_user_id"
  #  end
-puts "Cleaning User"
+
+puts "Cleaning Database"
+ Booking.destroy_all
+ Book.destroy_all
  User.destroy_all
 
-puts "Creating 5 Smart Seeds"
+puts "Creating Smart Seeds"
 
 my_user = User.create!(
   name: "John Doe",
@@ -28,8 +31,33 @@ my_user = User.create!(
   password: "livingyokolife"
   )
 
-puts "Cleaning database"
-Book.destroy_all
+mylene = User.create!(
+  name: "Mylene",
+  address: "101 Mylene St",
+  email: "mylene@me.org",
+  password: "mylenelife"
+  )
+
+troy = User.create!(
+  name: "troy",
+  address: "101 troy St",
+  email: "troy@me.org",
+  password: "troylife"
+  )
+
+spencer = User.create!(
+  name: "spencer",
+  address: "101 spencer St",
+  email: "spencer@me.org",
+  password: "spencerlife"
+  )
+
+dean = User.create!(
+  name: "dean",
+  address: "101 dean St",
+  email: "dean@me.org",
+  password: "deanlife"
+  )
 
 Book.create!(
   title: "Corporate Crime",
@@ -43,28 +71,28 @@ Book.create!(
 Book.create!(
   title: "Harry Potter and the Philosopher's Stone",
   description: "Harry Potter spent ten long years living with Mr. and Mrs. Dursley, an aunt and uncle whose outrageous favoritism of their perfectly awful son Dudley leads to some of the most inspired dark comedy since Charlie and the Chocolate Factory.",
-  category: "Fantasy",
+  category: "Sci-Fi and Fantasy",
   cover_picture: "https://images-na.ssl-images-amazon.com/images/I/51mtZy7oJVL._SX336_BO1,204,203,200_.jpg",
   ISBN: 9780590353403,
-  user: my_user
+  user: mylene
   )
 
 Book.create!(
   title: "The Fellowship of the Ring: LOTR Book 1",
   description: "Immerse yourself in Middle-earth with Tolkien's classic masterpieces behind the films, telling the complete story of Bilbo Baggins and the Hobbits' epic encounters with Gandalf, Gollum, dragons and monsters, in the quest to destroy the One Ring",
-  category: "Fantasy",
+  category: "Sci-Fi and Fantasy",
   cover_picture: "https://images-na.ssl-images-amazon.com/images/I/91dR5VlLTVL.jpg",
   ISBN: 9780007488315,
-  user: my_user
+  user: troy
   )
 
 Book.create!(
   title: "A Game of Thrones: A Song of Ice and Fire: Book One",
   description: "Magic, mystery, intrigue, romance, and adventure fill these pages and transport us to a world unlike any we have ever experienced.",
-  category: "Fantasy",
+  category: "Sci-Fi and Fantasy",
   cover_picture: "https://www.coverwhiz.com/uploads/tv/Game-of-Thrones-Season-8_v3.jpg",
   ISBN: 9780553897845,
-  user: my_user
+  user: spencer
   )
 
 Book.create!(
@@ -73,7 +101,7 @@ Book.create!(
   category: "Kids",
   cover_picture: "http://books.google.com/books/content?id=HshOnwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
   ISBN: 9780689866234,
-  user: my_user
+  user: dean
   )
 
 Book.create!(
@@ -82,16 +110,16 @@ Book.create!(
   category: "Romance",
   cover_picture: "http://books.google.com/books/content?id=G_xavgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
   ISBN: 9781612130286,
-  user: my_user
+  user: mylene
   )
 
 Book.create!(
   title: "The Martian",
   description: "A space mission takes an unexpected turn",
-  category: "Sci-Fi",
+  category: "Sci-Fi and Fantasy",
   cover_picture: "http://books.google.com/books/content?id=53pOCgAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
   ISBN: 9781785031137,
-  user: my_user
+  user: troy
   )
 
 Book.create!(
@@ -100,7 +128,7 @@ Book.create!(
   category: "Classics",
   cover_picture: "http://books.google.co.jp/books?id=2xm9oQEACAAJ&dq=isbn:9780333791035&hl=&cd=1&source=gbs_api",
   ISBN: 9780333791035,
-  user: my_user
+  user: spencer
   )
 
 Book.create!(
@@ -109,7 +137,7 @@ Book.create!(
   category: "Comics",
   cover_picture: "http://books.google.com/books/content?id=PR9dwwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
   ISBN: 1401294847,
-  user: my_user
+  user: dean
   )
 
 Book.create!(
@@ -118,7 +146,7 @@ Book.create!(
   category: "Mystery and Crime",
   cover_picture: "http://books.google.com/books/content?id=e9GNwy_vW10C&printsec=frontcover&img=1&zoom=1&source=gbs_api",
   ISBN: 9780062074003,
-  user: my_user
+  user: mylene
   )
 
 puts "Smart seeds created"
