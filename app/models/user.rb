a852da6c49a597c9293b2c58e52c098fa094e402
Dean Abbott <deanabbott293@gmail.com>
2021-07-20 21:04:57 +0900

    deleted dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings # bookings as a client
   has_many :books
-  has_many :bookings_as_owner, through: :books, source: :bookings, dependent: :destroy
+  has_many :bookings_as_owner, through: :books, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
