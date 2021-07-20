1c1afd1294f1e1ff2b66cd84c0ce33cdaf52ce85
Dean Abbott <deanabbott293@gmail.com>
2021-07-20 15:51:01 +0900

    basic pundit setup

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,6 @@ class BooksController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @books = Book.all
+    @books = policy_scope(Book).order(created_at: :desc)
   end
 end
