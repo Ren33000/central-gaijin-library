cf19025587fe2a099846c7f126d0aa94eaa19a7b
Dean Abbott <deanabbott293@gmail.com>
2021-07-29 16:58:52 +0900

    can see own bookings and requests in separate tabs

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,7 @@ class DashboardsController < ApplicationController
   def profile
     @bookings = Booking.where(user_id: current_user.id).order(created_at: :desc)
     authorize @bookings
+    @requests = current_user.bookings_as_owner
   end
+
 end
