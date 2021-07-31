78ade6f52e39765d2abcd259de0164308f7da832
Dean Abbott <deanabbott293@gmail.com>
2021-07-31 16:38:15 +0900

    form js fixsed

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,3 +1,5 @@
+import flatpickr from "flatpickr";
+
 const unavailableBookingDates = () => {
   // First we define two variables that are going to grab our inputs field. You can check the ids of the inputs with the Chrome inspector.
   const startDateInput = document.getElementById('booking_start_date');
